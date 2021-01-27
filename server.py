d4a16a424c11b334d0078463dd00e288cad70c6a
dododoSA <dododosa14@gmail.com>
2021-01-28 01:24:40 +0900

    initial commit

---
+++ server.py
@@ -0,0 +1,26 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("piano.html")
+
+@app.route("/freq", methods=["GET", "POST"])
+def getAction():
+    if request.method == "GET":
+        f = open('./data.txt', 'r', encoding='UTF-8')
+        data = f.read()
+        f.close()
+        return data
+
+    if request.method == "POST":
+        payload = request.json
+        freq = str(payload.get('freq'))
+        print(freq)
+        f = open('./data.txt', 'w', encoding='UTF-8')
+        f.write(freq)
+        f.close()
+        return freq
+
+
+app.run(host="127.0.0.1", port=8000)
